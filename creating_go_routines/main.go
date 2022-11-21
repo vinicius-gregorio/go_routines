03be24746fd673bdea59dea0f0b21645c629a294
Vinicius Gregorio <vinicius.squadp@genial.com.vc>
2022-11-20 22:58:01 -0300

    add wait groups

--- creating_go_routines/main.go
+++ creating_go_routines/main.go
@@ -1,11 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"sync"
+)
 
-func printSomething(s string) {
+func printSomething(s string, wg *sync.WaitGroup) {
+	defer wg.Done()
 	fmt.Println(s)
 }
 func main() {
-	printSomething("First print")
-	printSomething("Second print")
+	var wg sync.WaitGroup
+	words := []string{"alpha", "beta", "gamma", "delta", "epsilon"}
+
+	wg.Add(len(words))
+
+	for i, v := range words {
+		go printSomething(strconv.Itoa(i)+"  "+string(v), &wg)
+	}
+
+	wg.Wait()
+
+	wg.Add(1)
+	printSomething("First print", &wg)
 }
